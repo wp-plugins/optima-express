bc10b0b576e982f09f79693e018e3360d2edfc96
ihomefinder <ihomefinder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-29 21:00:33 +0000

    Fixed a bug in the getKey method.
    git-svn-id: https://plugins.svn.wordpress.org/optima-express/trunk@975497 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- iHomefinderCacheUtility.php
+++ iHomefinderCacheUtility.php
@@ -6,18 +6,18 @@ if( !class_exists('IHomefinderCacheUtility')) {
 	 * @author ihomefinder
 	 */
 	class IHomefinderCacheUtility {
-		
+
 		/*
 		prefix should only be up 13 character in length because
 		cache key can only be 45 characters. prefix (13) + md5
-		hash (32). 
+		hash (32).
 		*/
 		const CACHE_PREFIX = "ihf_cache_";
-		
+
 		public function __construct() {
-			
+
 		}
-		
+
 		public function getItem( $key ) {
 			$cacheKey = $this->getKey( $key );
 			IHomefinderLogger::getInstance()->debug( 'get cached version cacheKey ' . $cacheKey );
@@ -26,27 +26,27 @@ if( !class_exists('IHomefinderCacheUtility')) {
 			IHomefinderLogger::getInstance()->debug( 'value ' . $value );
 			return $value;
 		}
-		
+
 		public function updateItem( $key, $value, $expiration ) {
 			$cacheKey = $this->getKey( $key );
 			IHomefinderLogger::getInstance()->debug( 'updating cache cacheKey ' . $cacheKey );
 			// Set a new transient
 			set_transient( $cacheKey, $value, $expiration );
 		}
-		
+
 		public function deleteItem( $key ) {
 			$cacheKey = $this->getKey( $key );
 			IHomefinderLogger::getInstance()->debug( 'deleting cache cacheKey ' . $cacheKey );
 			// Delete a transient
 			delete_transient($cacheKey);
 		}
-		
+
 		private function getKey( $key ) {
 			$keyHash = md5( $key );
-			$cacheKey = $this::CACHE_PREFIX .  $keyHash;
+			$cacheKey = $this->CACHE_PREFIX .  $keyHash;
 			return $cacheKey;
 		}
-		
+
 	}
 }
 ?>
